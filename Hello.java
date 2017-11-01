818ac856c0683c23ff0176d6cf44205ac000f09a
Xinyi Guo <xig080@ucsd.edu>
2017-11-01 15:52:34 -0700

    Add an author/email comment

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello
     public static void main( String argv[] )
     {
         // Default is "World"
+        // Author: Jim Weirich (jim@somewhere.com)
         String name = "World";
         if ( argv.length != 0 )
         {
