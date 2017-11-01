5e24e1eb621b2b77ef6ad68b2e98f28c1b864923
Xinyi Guo <xig080@ucsd.edu>
2017-11-01 16:03:05 -0700

    Hello uses Greeter

--- lib/Hello.java
+++ lib/Hello.java
@@ -3,13 +3,12 @@ public class Hello
     public static void main( String argv[] )
     {
         // Default is "World"
-        // Author: Jim Weirich (jim@somewhere.com)
         String name = "World";
         if ( argv.length != 0 )
         {
             name = argv[0];
         }
-
+        Greeter greeter = new Greeter(name);
         System.out.println( "Hello, " + name + "!" );    
 }
 }
